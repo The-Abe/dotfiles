70b0b5fc2706fb9e8a31d2c19a84b6ac9726dbd0
Abe <abevanderwielen@gmail.com>
2023-11-23 14:44:36 +0100

    Fix harpoon shortcuts and new ones for file 1,2 and 3

--- vim/lua/plugins/harpoon.lua
+++ vim/lua/plugins/harpoon.lua
@@ -14,7 +14,10 @@ return {
 			vim.keymap.set('n', '<leader>hx', require('harpoon.mark').add_file, { silent = true, desc = 'Add mark' })
 			vim.keymap.set('n', '<leader>hn', require('harpoon.ui').nav_next, { silent = true, desc = 'Next' })
 			vim.keymap.set('n', '<leader>hp', require('harpoon.ui').nav_prev, { silent = true, desc = 'Previous' })
-			vim.keymap.set('n', "<leader>hh", ':Telescope harpoon marks<CR>', { silent = true, desc = 'Marks' })
+			vim.keymap.set('n', '<leader>h1', ':lua require("harpoon.ui").nav_file(1)<cr>', { silent = true, desc = 'Mark 1' })
+			vim.keymap.set('n', '<leader>h2', ':lua require("harpoon.ui").nav_file(2)<cr>', { silent = true, desc = 'Mark 2' })
+			vim.keymap.set('n', '<leader>h3', ':lua require("harpoon.ui").nav_file(3)<cr>', { silent = true, desc = 'Mark 3' })
+			vim.keymap.set('n', "<leader>hh", require('harpoon.ui').toggle_quick_menu, { silent = true, desc = 'Marks' })
 		end,
 	}
 }
