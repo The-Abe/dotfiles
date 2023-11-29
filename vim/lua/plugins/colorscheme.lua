bbfb3913f10b908f5630cf1a5e75b233d42fee58
Abe <abevanderwielen@gmail.com>
2023-11-29 17:19:31 +0100

    background color

--- vim/lua/plugins/colorscheme.lua
+++ vim/lua/plugins/colorscheme.lua
@@ -5,26 +5,31 @@ return {
 		config = function()
 			vim.cmd([[ colorscheme catppuccin-mocha ]])
 			require("catppuccin").setup({
-					flavour = "mocha", -- latte, frappe, macchiato, mocha
-					background = { -- :h background
-							light = "latte",
-							dark = "mocha",
+				flavour = "mocha", -- latte, frappe, macchiato, mocha
+				background = { -- :h background
+					light = "latte",
+					dark = "mocha",
+				},
+				transparent_background = true, -- disables setting the background color.
+				dim_inactive = {
+					enabled = true, -- dims the background color of inactive window
+					shade = "dark",
+					percentage = 0.15, -- percentage of the shade to apply to the inactive window
+				},
+				no_italic = true, -- Force no italic
+				no_bold = true, -- Force no bold
+				no_underline = false, -- Force no underline
+				integrations = {
+					cmp = true,
+					gitsigns = true,
+					nvimtree = true,
+					treesitter = true,
+				},
+				color_overrides = {
+					all = {
+						base = "#262626",
 					},
-					transparent_background = true, -- disables setting the background color.
-					dim_inactive = {
-							enabled = true, -- dims the background color of inactive window
-							shade = "dark",
-							percentage = 0.15, -- percentage of the shade to apply to the inactive window
-					},
-					no_italic = true, -- Force no italic
-					no_bold = true, -- Force no bold
-					no_underline = false, -- Force no underline
-					integrations = {
-							cmp = true,
-							gitsigns = true,
-							nvimtree = true,
-							treesitter = true,
-					},
+				},
 			})
 		end,
 		init = function()
