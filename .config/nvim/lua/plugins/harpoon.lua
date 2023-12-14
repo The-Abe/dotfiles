d3908710c5f41517e486a6571f43d9f58dcbfae6
The-Abe <abevanderwielen@gmail.com>
2023-12-14 13:18:12 +0100

    Added <leader>1,2,3,4,5 mappings for faster harpooning

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -14,9 +14,11 @@ return {
 			vim.keymap.set('n', '<leader>hx', require('harpoon.mark').add_file, { silent = true, desc = 'Add mark' })
 			vim.keymap.set('n', '<leader>hn', require('harpoon.ui').nav_next, { silent = true, desc = 'Next' })
 			vim.keymap.set('n', '<leader>hp', require('harpoon.ui').nav_prev, { silent = true, desc = 'Previous' })
-			vim.keymap.set('n', '<leader>h1', ':lua require("harpoon.ui").nav_file(1)<cr>', { silent = true, desc = 'Mark 1' })
-			vim.keymap.set('n', '<leader>h2', ':lua require("harpoon.ui").nav_file(2)<cr>', { silent = true, desc = 'Mark 2' })
-			vim.keymap.set('n', '<leader>h3', ':lua require("harpoon.ui").nav_file(3)<cr>', { silent = true, desc = 'Mark 3' })
+			vim.keymap.set('n', '<leader>1', ':lua require("harpoon.ui").nav_file(1)<cr>', { silent = true, desc = 'Mark 1' })
+			vim.keymap.set('n', '<leader>2', ':lua require("harpoon.ui").nav_file(2)<cr>', { silent = true, desc = 'Mark 2' })
+			vim.keymap.set('n', '<leader>3', ':lua require("harpoon.ui").nav_file(3)<cr>', { silent = true, desc = 'Mark 3' })
+			vim.keymap.set('n', '<leader>4', ':lua require("harpoon.ui").nav_file(4)<cr>', { silent = true, desc = 'Mark 4' })
+			vim.keymap.set('n', '<leader>5', ':lua require("harpoon.ui").nav_file(5)<cr>', { silent = true, desc = 'Mark 5' })
 			vim.keymap.set('n', "<leader>hh", require('harpoon.ui').toggle_quick_menu, { silent = true, desc = 'Marks' })
 		end,
 	}
