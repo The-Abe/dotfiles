62cec2c9b8fb2b718ee50c43684c8ec2598b248c
Abe <abevanderwielen@gmail.com>
2024-01-16 17:15:58 +0100

    Background color change

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -1,15 +1,17 @@
 return {
-	'sainnhe/gruvbox-material',
-	config = function ()
-		vim.g.gruvbox_material_background = 'hard'
-		vim.g.gruvbox_material_better_performance = 1
-		vim.g.gruvbox_material_transparent_background = 0
-		vim.g.gruvbox_material_dim_inactive_windows = 1
-		vim.g.gruvbox_material_ui_contrast = 'high'
-		vim.g.gruvbox_material_show_eob = 0
-		vim.g.gruvbox_material_float_style = 'dim'
-		vim.cmd([[colorscheme gruvbox-material]])
-		vim.cmd([[hi Normal guibg=#1c1c1c ]])
-		vim.cmd('hi TelescopeBorder guifg=#e78a4e guibg=#1c1c1c')
-	end
+	{
+		'sainnhe/gruvbox-material',
+		config = function ()
+			vim.g.gruvbox_material_backgrounk = 'hard'
+			vim.g.gruvbox_material_better_performance = 1
+			vim.g.gruvbox_material_transparent_background = 0
+			vim.g.gruvbox_material_dim_inactive_windows = 1
+			vim.g.gruvbox_material_ui_contrast = 'high'
+			vim.g.gruvbox_material_show_eob = 0
+			vim.g.gruvbox_material_float_style = 'dim'
+			vim.cmd([[colorscheme gruvbox-material]])
+			vim.cmd('hi TelescopeBorder guifg=#e78a4e guibg=#282828')
+		end
+	},
+
 }
