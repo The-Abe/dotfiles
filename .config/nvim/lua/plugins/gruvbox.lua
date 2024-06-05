c9c376f56c1f8c3c7a489fcddf3dbc461ac7618f
The-Abe <abevanderwielen@gmail.com>
2024-06-05 10:48:49 +0200

    Laptop changes to config

--- .config/nvim/lua/plugins/gruvbox.lua
+++ .config/nvim/lua/plugins/gruvbox.lua
@@ -4,7 +4,7 @@ return {
 		config = function ()
 			vim.g.gruvbox_material_backgrounk = 'hard'
 			vim.g.gruvbox_material_better_performance = 1
-			vim.g.gruvbox_material_transparent_background = 1
+			vim.g.gruvbox_material_transparent_backgrounk = 0
 			vim.g.gruvbox_material_dim_inactive_windows = 1
 			vim.g.gruvbox_material_ui_contrast = 'high'
 			vim.g.gruvbox_material_show_eob = 0
