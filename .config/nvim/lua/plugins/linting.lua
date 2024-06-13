14a18ae75eabac38869efb9eabd2cc4dd7a2741c
The-Abe <abevanderwielen@gmail.com>
2024-06-13 16:05:32 +0200

    nvim-lint config

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -7,9 +7,12 @@ return {
 	config = function()
 		local lint = require("lint")
 		lint.linters_by_ft = {
-			markdown = { 'vale', },
+			markdown = { 'markdownlint' },
 			yaml = { 'ansible_lint', 'yamllint' },
 			ruby = { 'standardrb' },
+			sh = { 'shellcheck' },
+			json = { 'jsonlint' },
+			go = { 'golangci-lint' },
 		}
 		local lint_auggroup = vim.api.nvim_create_augroup('lint', { clear = true })
 		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
